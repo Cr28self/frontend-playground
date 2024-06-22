819091be5b4a6417360288305f51aee720c74aec
Cr28self <tkhoon3study@gmail.com>
2024-06-22 21:47:50 +0900

    fix : delete build-demo.tsx docker 이미지 빌드 성공

--- src/routes/builder-demo.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import BuilderPage from '../builder-page'
-
-export const Route = createFileRoute('/builder-demo')({
-  component: BuilderPage
-})
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { builderDevTools } from "@builder.io/dev-tools/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), builderDevTools()],
+  plugins: [react(), TanStackRouterVite()],
 
   resolve: {
     alias: {
